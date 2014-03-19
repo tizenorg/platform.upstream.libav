2b42ac1cf83f558249f90813c5585270d9ccd135
Kitae Kim <kt920.kim@samsung.com>
2014-03-19 20:12:51 +0900

    add disable-yasm option
    yasm is not installed in default on each platform and disable yasm option
    until checking that its option improves performance of video decoders.
    
    Change-Id: If6c8c240e4db796ba5fa86945e236991493ed7e1
    Signed-off-by: Kitae Kim <kt920.kim@samsung.com>

--- package/emulator_configure.sh
+++ package/emulator_configure.sh
@@ -3,15 +3,11 @@
 # OS specific
 echo "##### checking for os... targetos $targetos"
 targetos=`uname -s`
-bindir="binary"
+bin_dir=./
 
 cd ../
 echo ""
 echo "##### libav configure for emulator"
-if [ ! -e ${bindir} ];
-then
-	mkdir ${bindir}
-fi
 emul_suffix="-emul"
 append_configure=""
 
@@ -27,10 +23,10 @@ append_configure="--cc=cc --extra-cflags=-mmacosx-version-min=10.4"
 esac
 
 ./configure \
- --prefix=${bindir} --arch=x86 --build-suffix=${emul_suffix} \
+ --prefix=${bin_dir} --arch=x86 --build-suffix=${emul_suffix} \
  --disable-static --enable-shared --enable-pic --enable-optimizations \
  --disable-gpl --disable-doc --disable-avserver --disable-avplay --disable-avconv --disable-avprobe \
- --disable-everything --disable-network \
+ --disable-everything --disable-network --disable-yasm \
  --enable-encoder=aac --enable-encoder=h263 --enable-encoder=h263p \
  --enable-encoder=mpeg4 --enable-encoder=msmpeg4v2 --enable-encoder=msmpeg4v3 \
  --enable-decoder=aac --enable-decoder=mp3 --enable-decoder=mp3adu \
