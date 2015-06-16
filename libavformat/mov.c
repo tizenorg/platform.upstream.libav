d503b31b220b22ac946a7003d37ef8cf09ed8557
ji.yong.seo <ji.yong.seo@samsung.com>
2015-06-16 14:38:02 +0900

    Videos that contain cdis, get duration fail issue. (patch from latest ffmpeg)
    Change-Id: I5d9c9c3d0797e7d719019504052bac017b861518

--- libavformat/mov.c
+++ libavformat/mov.c
@@ -812,6 +812,10 @@ static int mov_read_mvhd(MOVContext *c, AVIOContext *pb, MOVAtom atom)
     av_dlog(c->fc, "time scale = %i\n", c->time_scale);
 
     c->duration = (version == 1) ? avio_rb64(pb) : avio_rb32(pb); /* duration */
+    // set the AVCodecContext duration because the duration of individual tracks
+    // may be inaccurate
+    if (c->time_scale > 0)
+        c->fc->duration = av_rescale(c->duration, AV_TIME_BASE, c->time_scale);
     avio_rb32(pb); /* preferred scale */
 
     avio_rb16(pb); /* preferred volume */
